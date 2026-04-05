011a7bd115d74884c45ae4c1eacdaccf4726837b
quanhm <hamanhquanqc@gmail.com>
2026-04-05 11:57:09 +0700

    update app.css

--- src/App.js
+++ src/App.js
@@ -1,61 +1,63 @@
 import './App.css';
 
-import React, { useState } from "react"; // Bổ sung useState
-import { Grid, Typography, Paper } from "@mui/material";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useState } from "react";
+import { Grid, Paper } from "@mui/material";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 
 import TopBar from "./components/TopBar";
 import UserDetail from "./components/UserDetail";
 import UserList from "./components/UserList";
 import UserPhotos from "./components/UserPhotos";
 
-const App = (props) => {
-  // Thêm state để quản lý tính năng Advanced Features (Extra Credit)
+const App = () => {
+  // Trạng thái bật/tắt tính năng nâng cao (Extra Credit)
   const [advancedFeatures, setAdvancedFeatures] = useState(false);
 
   return (
-      <Router>
-        <div>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              {/* Truyền state và hàm set xuống TopBar để điều khiển Checkbox */}
-              <TopBar 
-                advancedFeatures={advancedFeatures} 
-                setAdvancedFeatures={setAdvancedFeatures} 
-              />
-            </Grid>
-            <div className="main-topbar-buffer" />
-            <Grid item sm={3}>
-              <Paper className="main-grid-item">
-                <UserList />
-              </Paper>
-            </Grid>
-            <Grid item sm={9}>
-              <Paper className="main-grid-item">
-                <Routes>
-                  <Route
-                      path="/users/:userId"
-                      element={<UserDetail />}
-                  />
-                  
-                  {/* Cấu hình 2 Route cho UserPhotos để hỗ trợ Deep Linking.
-                      Một cái có photoId, một cái không có, cả hai đều trỏ về 1 Component */}
-                  <Route
-                      path="/photos/:userId/:photoId"
-                      element={<UserPhotos advancedFeatures={advancedFeatures} />}
-                  />
-                  <Route
-                      path="/photos/:userId"
-                      element={<UserPhotos advancedFeatures={advancedFeatures} />}
-                  />
-                  
-                  <Route path="/users" element={<UserList />} />
-                </Routes>
-              </Paper>
-            </Grid>
+    <Router>
+      <div>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <TopBar 
+              advancedFeatures={advancedFeatures} 
+              setAdvancedFeatures={setAdvancedFeatures} 
+            />
           </Grid>
-        </div>
-      </Router>
+
+          <div className="main-topbar-buffer" />
+
+          <Grid item sm={3}>
+            <Paper className="main-grid-item">
+              <UserList />
+            </Paper>
+          </Grid>
+
+          <Grid item sm={9}>
+            <Paper className="main-grid-item">
+              <Routes>
+                <Route
+                  path="/users/:userId"
+                  element={<UserDetail />}
+                />
+
+                <Route
+                  path="/photos/:userId/:photoId"
+                  element={<UserPhotos advancedFeatures={advancedFeatures} />}
+                />
+                <Route
+                  path="/photos/:userId"
+                  element={<UserPhotos advancedFeatures={advancedFeatures} />}
+                />
+
+                <Route path="/users" element={<UserList />} />
+
+                <Route path="/" element={<Navigate to="/users" />} />
+              </Routes>
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
+    </Router>
   );
 }
 
